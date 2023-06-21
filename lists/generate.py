d77061fa970e94210fa5bfb34b9e95fee3a01ac6
Andrei Shevchuk <andrei@shevchuk.co>
2023-06-21 03:39:40 +0300

    Initial commit

---
+++ lists/generate.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+import os
+import glob
+from pathlib import Path
+
+
+apps = ['clash', 'switchyomega']
+lists = glob.glob('*.list')
+
+
+def generate_clash_list(in_file, out_file):
+  for line in in_file:
+    if line[0] == '#' or line.strip() == '': continue
+    out_file.write(f'+.{line}')
+
+
+def generate_switchyomega_list(in_file, out_file):
+  out_file.write('[SwitchyOmega Conditions]\n')
+
+  for line in in_file:
+    if line[0] == '#' or line.strip() == '': continue
+    out_file.write(f'*.{line}')
+
+
+os.chdir(Path(__file__).resolve().parent)
+
+for app in apps:
+  for list_file in lists:
+    filename = Path(list_file).stem
+    out_path = Path(app) / filename
+    in_file = open(list_file)
+    out_file = open(out_path, 'w')
+
+    locals()[f'generate_{app}_list'](in_file, out_file)
+
+    in_file.close()
+    out_file.close()
